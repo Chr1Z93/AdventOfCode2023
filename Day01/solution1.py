ac3f10c6430fc01001837f446015c36dc1c19488
Chr1Z93 <puls.christian@gmail.com>
2023-12-01 12:54:16 +0100

    added day01-1

---
+++ Day01/solution1.py
@@ -0,0 +1,34 @@
+# On each line, the calibration value can be found by combining the first digit
+# and the last digit (in that order) to form a single two-digit number.
+# What is the sum of all of the calibration values?
+
+from pathlib import Path
+
+# get correct subfolder path
+scriptPath = Path(__file__).resolve()
+scriptDir = scriptPath.parent
+inputPath = scriptDir / "input1.txt"
+
+f = open(inputPath)
+
+answer = 0
+firstChar = ""
+secondChar = ""
+
+for line in f:
+    # get first digit
+    for char in line:
+        if char.isdigit():
+            firstChar = char
+            break
+
+    # get last digit
+    for char in reversed(line):
+        if char.isdigit():
+            secondChar = char
+            break
+
+    # combine both digits and add to answer
+    answer += int(firstChar + secondChar)
+
+print(answer)
