85d096a0c3be5a54068d7b0ca1f8c082b9a6ddbd
Chr1Z93 <puls.christian@gmail.com>
2023-12-05 15:45:36 +0100

    before chainmap

--- Day05/solution1.py
+++ Day05/solution1.py
@@ -7,59 +7,66 @@
 
 from pathlib import Path
 import io
-import gc
+import time
+
+
+def generateMap(tempStr):
+    keyStart = int(tempStr[1])
+    keyCount = int(tempStr[2])
+    valueStart = int(tempStr[0])
+    return dict.fromkeys(range(keyStart, keyStart + keyCount), [valueStart, keyStart])
+
+
+def getValue(mapId, key):
+    value = mapOfMaps[mapId].get(key, key)
+    if value != key:
+        value = value[0] + key - value[1]
+    return value
+
+
+# get the start time
+start = time.time()
 
 # get correct subfolder path
 scriptPath = Path(__file__).resolve()
 scriptDir = scriptPath.parent
 inputPath = scriptDir / "input.txt"
 data = Path(inputPath).read_text()
+splitInput = data.split("\n\n")
 
 seeds = []
-mapOfMaps = {
-    1: {},
-    2: {},
-    3: {},
-    4: {},
-    5: {},
-    6: {},
-    7: {},
-}
+mapOfMaps = {}
 
+for i in range(1, 8):
+    mapOfMaps[i] = {}
 
-def fillMap(map, keyStart, keyCount, valueStart):
-    for i in range(keyStart, keyStart + keyCount):
-        map[i] = valueStart + i - keyStart
-
-
-splitInput = data.split("\n\n")
 for mapId, block in enumerate(splitInput):
-    print("mapId:", mapId)
     if mapId == 0:
         tempStr = block.split(":")
         seeds = tempStr[1].split()
     else:
+        map = mapOfMaps[mapId]
         for i, line in enumerate(io.StringIO(block)):
             if i != 0:
-                tempStr = line.split()
-                map = mapOfMaps[mapId]
-                fillMap(map, int(tempStr[1]), int(tempStr[2]), int(tempStr[0]))
-                gc.collect()
+                newMap = generateMap(line.split())
+                map.update(newMap)
+    print("mapId:", mapId, "Time:", time.time() - start)
 
 answer = 0
 for seed in seeds:
     num = int(seed)
-    result1 = mapOfMaps[1].get(num, num)
-    result2 = mapOfMaps[2].get(result1, result1)
-    result3 = mapOfMaps[3].get(result2, result2)
-    result4 = mapOfMaps[4].get(result3, result3)
-    result5 = mapOfMaps[5].get(result4, result4)
-    result6 = mapOfMaps[6].get(result5, result5)
-    result7 = mapOfMaps[7].get(result6, result6)
+    result1 = getValue(1, num)
+    result2 = getValue(2, result1)
+    result3 = getValue(3, result2)
+    result4 = getValue(4, result3)
+    result5 = getValue(5, result4)
+    result6 = getValue(6, result5)
+    result7 = getValue(7, result6)
 
-    print("Seed:", num)
-    print("Conversion:", result1, result2, result3, result4, result5, result6, result7)
+    # print("Seed:", num)
+    # print("Conversion:", result1, result2, result3, result4, result5, result6, result7)
 
     if answer == 0 or answer > result7:
         answer = result7
 print("Answer:", answer)
+print("Time: ", time.time() - start)
